79500d88a661acb5ee23b47183f50bb9c0859ecc
boyarishnik <boyarishnik3232@gmail.com>
2025-08-09 21:20:09 +0300

    asdasd

---
+++ src/index/index.rs
@@ -0,0 +1,30 @@
+use std::{fs::OpenOptions, io::{BufRead, BufReader, BufWriter, Read, Seek, Write}, path::{Path, PathBuf}};
+
+use crate::{base::GILLTTER_PATH, gilltter_add, objects::{self, tree::{Object, ObjectType}}};
+
+pub fn add_one_in_index(filepath: &Path) -> anyhow::Result<()> {
+    let index_file = OpenOptions::new().write(true).read(true).create(true).open(Path::new(GILLTTER_PATH).join("index"))?;
+
+    let mut reader = BufReader::new(index_file.try_clone()?);
+    
+    let mut start_index = 0;
+    for i in reader.by_ref().lines() {
+        if let Ok(s) = i {
+            let s_ = s.split_ascii_whitespace().collect::<Vec<&str>>();
+            let thing_type = s_[1];
+            let name = PathBuf::from(&s_[2..].join(" "));
+            if filepath.parent().unwrap() == name.parent().unwrap() {
+                break
+            }
+            println!("{:?} {:?}", filepath, name);
+            start_index += s.len() + 1;
+        }
+    }
+    
+    let utils_sha1 = gilltter_add(filepath.to_str().unwrap());
+
+    let mut writer = BufWriter::new(index_file);
+    writer.seek(std::io::SeekFrom::Start(start_index as u64))?;
+    writer.write_all(format!("{} {} {}\n", utils_sha1, "blob", filepath.to_str().unwrap()).as_bytes())?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::{
     collections::HashMap,
     fs::File,
-    io::{Read, Write},
+    io::{Read, Write}, path::Path,
 };
+
+use clap::{Arg, ArgAction, Command};
 
 use crate::{
     base::{GILLTER_OBJECTS_DIR, GILLTTER_PATH},
@@ -112,69 +114,101 @@ fn config_mock() {
 }
 
 fn main() {
-    gilltter_init();
-
-    let mut index_mock: HashMap<String, Object> = HashMap::new();
-
-    // Imagine tree constructing when commiting
-    {
-        let utils_filepath = String::from("src/utils.rs");
-        let utils_sha1 = gilltter_add(&utils_filepath);
-        index_mock.insert(
-            utils_sha1.clone(),
-            Object::new(
-                objects::tree::ObjectType::Blob,
-                utils_filepath.to_string(),
-                utils_sha1.clone(),
-            ),
+    let mut app = Command::new("gilltter")
+        .version("0.1")
+        .about("Simple version control system on Rust")
+        .subcommand(Command::new("init").about("Initialize gilltter repo"))
+        .subcommand(
+            Command::new("add").about("Adding file to index").args([
+                Arg::new("all")
+                    .short('a')
+                    .long("all")
+                    .action(ArgAction::SetTrue),
+                Arg::new("filename").index(1),
+            ]),
         );
+    
+    let help = app.render_help();
+    let args = app.get_matches();
+    let command = args.subcommand().unwrap();
 
-        let base_filepath = String::from("src/base.rs");
-        let base_sha1 = gilltter_add(&base_filepath);
-        index_mock.insert(
-            base_sha1.clone(),
-            Object::new(
-                objects::tree::ObjectType::Blob,
-                base_filepath.to_string(),
-                base_sha1.clone(),
-            ),
-        );
-
-        // Build a tree
-        let mut tree = Tree::new();
-
-        for (_, value) in index_mock.into_iter() {
-            tree.add_object(Object::new(
-                ObjectType::Blob,
-                value.filepath,
-                value.sha1_pointer,
-            ));
+    match command.0 {
+        "init" => gilltter_init(),
+        "add" => {
+            if *command.1.get_one::<bool>("all").unwrap() {
+                println!("add all");
+            } else if let Some(filename) = command.1.get_one::<String>("filename") {
+                println!("add {filename}");
+                index::index::add_one_in_index(Path::new(filename)).unwrap();
+            } else {
+                print!("{help}");
+            }
         }
-
-        let tree_name = tree.dump_to_file().unwrap();
-
-        let tree = Tree::from_file(&format!(".gilltter/objects/{}", tree_name)).unwrap();
-
-        // now build a commit
-        // we need tree, parent, user, message in commit
-        let config = Config::from_file(".gilltter/config").unwrap();
-        let username = config.get("Account", "username").unwrap();
-        let email = config.get("Account", "email").unwrap();
-
-        let mut commit = Commit::new();
-        commit.set_tree_sha(tree_name);
-        commit.set_username(username);
-        commit.set_email(email);
-        commit.set_message("fuck niggas");
-
-        let commit_name = commit.dump_to_file().unwrap();
-
-        let commit = Commit::from_file(&format!(".gilltter/objects/{}", commit_name)).unwrap();
-        println!(
-            "'{}' '{}' '{}'",
-            commit.get_email().unwrap(),
-            commit.get_username().unwrap(),
-            commit.get_message().unwrap()
-        );
+        _ => (), // unreachable
     }
+    
+
+    // let mut index_mock: HashMap<String, Object> = HashMap::new();
+
+    // // Imagine tree constructing when commiting
+    // {
+    //     let utils_filepath = String::from("src/utils.rs");
+    //     let utils_sha1 = gilltter_add(&utils_filepath);
+    //     index_mock.insert(
+    //         utils_sha1.clone(),
+    //         Object::new(
+    //             objects::tree::ObjectType::Blob,
+    //             utils_filepath.to_string(),
+    //             utils_sha1.clone(),
+    //         ),
+    //     );
+
+    //     let base_filepath = String::from("src/base.rs");
+    //     let base_sha1 = gilltter_add(&base_filepath);
+    //     index_mock.insert(
+    //         base_sha1.clone(),
+    //         Object::new(
+    //             objects::tree::ObjectType::Blob,
+    //             base_filepath.to_string(),
+    //             base_sha1.clone(),
+    //         ),
+    //     );
+
+    //     // Build a tree
+    //     let mut tree = Tree::new();
+
+    //     for (_, value) in index_mock.into_iter() {
+    //         tree.add_object(Object::new(
+    //             ObjectType::Blob,
+    //             value.filepath,
+    //             value.sha1_pointer,
+    //         ));
+    //     }
+
+    //     let tree_name = tree.dump_to_file().unwrap();
+
+    //     let tree = Tree::from_file(&format!(".gilltter/objects/{}", tree_name)).unwrap();
+
+    //     // now build a commit
+    //     // we need tree, parent, user, message in commit
+    //     let config = Config::from_file(".gilltter/config").unwrap();
+    //     let username = config.get("Account", "username").unwrap();
+    //     let email = config.get("Account", "email").unwrap();
+
+    //     let mut commit = Commit::new();
+    //     commit.set_tree_sha(tree_name);
+    //     commit.set_username(username);
+    //     commit.set_email(email);
+    //     commit.set_message("fuck niggas");
+
+    //     let commit_name = commit.dump_to_file().unwrap();
+
+    //     let commit = Commit::from_file(&format!(".gilltter/objects/{}", commit_name)).unwrap();
+    //     println!(
+    //         "'{}' '{}' '{}'",
+    //         commit.get_email().unwrap(),
+    //         commit.get_username().unwrap(),
+    //         commit.get_message().unwrap()
+    //     );
+    // }
 }
